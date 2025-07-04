9a76374b2a8a4c83d5f3c6af06c3d70b91a00ca0
DivyanshModi25 <divyanshmodi2510@gmail.com>
2025-07-04 13:20:55 +0530

    kafka ec2 changed to t3.small

--- cloud config/kafka_ec2.tf
+++ cloud config/kafka_ec2.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "kafka_sg" {
 
 resource "aws_instance" "kafka_ec2" {
   ami                    = "ami-0f918f7e67a3323f0" # Ubuntu 24.04 LTS in ap-south-1 (Mumbai)
-  instance_type          = "t3.medium"
+  instance_type          = "t3.small"
   subnet_id              = aws_subnet.private_subnet1.id 
   vpc_security_group_ids = [aws_security_group.kafka_sg.id]
   associate_public_ip_address = false 
